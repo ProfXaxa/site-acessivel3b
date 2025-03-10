46c65f6099aa1999f217157f7cb56ac5d6c438a5
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2025-03-10 08:54:42 -0300

    rem

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', function(){
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function( ){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     });
 });
